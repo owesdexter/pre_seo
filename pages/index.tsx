87ee6fb863a0bd08eca14f0dcadc9d977d436701
Dexter Chen <dexterchen@DexterdeMacBook-Pro.local>
2023-01-07 14:11:22 +0800

    fix domain

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,10 @@ export default function Home() {
           name="google-site-verification"
           content="WjmVbeylZsRzN_cs6eE1WK2kn_CUKQzv5oKvquQw-30"
         />
-        <link rel="canonical" href="pre-seo-ace-test.firebaseapp.com" />
+        <link
+          rel="canonical"
+          href="https://pre-seo-ace-test.firebaseapp.com/"
+        />
       </Head>
       <main className="title">
         <h1>阿德虛擬貨幣交易所 首頁</h1>
